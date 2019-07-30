dd54cc33aaf91012a6c6ac65acf09e540511313d
Matt Tedder <matthew.tedder@techtonic.com>
2019-07-30 09:31:56 -0600

    Lift state from Square to parent Board component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
     render() {
       return (
-        <button className="square"
-        onClick={() =>  this.setState({value: 'X'})}
+        <button
+          className="square"
+          onClick={() =>  this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
