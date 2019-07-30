65050252228b6513a209e4d14a4983ee474c398b
Matt Tedder <matthew.tedder@techtonic.com>
2019-07-30 10:03:20 -0600

    alternate turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,15 +5,19 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
-    console.log(squares)
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+    console.log(this.state)
   }
 
   renderSquare(i) {
@@ -26,7 +30,7 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
