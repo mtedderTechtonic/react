247691778f3616abdf9a9c6a426362eb657f3d9b
Matt Tedder <matthew.tedder@techtonic.com>
2019-07-30 08:52:34 -0600

    add value prop to Square component, add event listener

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() =>  alert('click')}>
+          {this.props.value}
         </button>
       );
     }
