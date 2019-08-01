adfdf1fa56c77fe11e0699366d1a2f64abb71827
Matt Tedder <matthew.tedder@techtonic.com>
2019-07-31 20:39:17 -0600

    Show moves history

--- src/App.jsx
+++ src/App.jsx
@@ -60,10 +60,22 @@ export default class App extends PureComponent {
       xIsNext: !this.state.xIsNext
     });
   }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = this.calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
     let status;
 
     if (winner) {
@@ -79,7 +91,7 @@ export default class App extends PureComponent {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
